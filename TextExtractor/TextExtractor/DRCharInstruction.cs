b0d793ba8619f7aafdc149d90159ef6b24e46f2e
Souzooka <bazookaharwell@gmail.com>
2018-04-30 14:43:39 -1000

    Restructure DR Char and use marshalling to convert bytes to struct

---
+++ TextExtractor/TextExtractor/DRCharInstruction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextExtractor
+{
+#pragma warning disable 0649
+    [StructLayout(LayoutKind.Sequential)]
+    struct DRCharInstruction
+    {
+        public short Value;
+        public short DRValue;
+        public byte Advance;
+        public byte Flags;
+
+        public bool EOL
+        {
+            get
+            {
+                return (Flags & 4) != 0;
+            }
+        }
+    }
+#pragma warning restore 0649
+}
+
--- TextExtractor/TextExtractor/Program.cs
+++ TextExtractor/TextExtractor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
@@ -11,18 +12,34 @@ namespace TextExtractor
     {
         static void Main(string[] args)
         {
-            byte[] contents = File.ReadAllBytes("shop_u16_usa.msg");
+            byte[] contents = File.ReadAllBytes("epilogue_u16_usa.rMessage");
             StringBuilder parsedText = new StringBuilder();
             int start = (int)contents[4];
 
-            for (int i = start; i < contents.Length; i += 6)
+            for (int i = start; i <= contents.Length - 6; i += 6)
             {
-                MyChar character = new MyChar(new byte[] { contents[i], contents[i + 1], contents[i + 2], contents[i + 3], contents[i + 4], contents[i + 5] }, "X360Beta");
-                parsedText.Append(character.Value);
+                // Get contents for 1 instruction in new array
+                byte[] data = new byte[6];
+                Array.Copy(contents, i, data, 0, 6);
+
+                // Get pointer to struct represented by array
+                DRCharInstruction character = ByteArrayToStructure<DRCharInstruction>(data);
+
+                // Parse character instruction data
                 if (character.EOL) { parsedText.Append('\n'); }
+                else { parsedText.Append((char)character.Value); }
             }
 
-            File.WriteAllText("./shop_u16_usa.txt", parsedText.ToString());
+            File.WriteAllText("./epilogue_u16_usa.txt", parsedText.ToString());
+        }
+
+        static T ByteArrayToStructure<T>(byte[] bytes) where T : struct
+        {
+            IntPtr ptr = Marshal.AllocHGlobal(bytes.Length);
+            Marshal.Copy(bytes, 0, ptr, bytes.Length);
+            T structureObj = (T)Marshal.PtrToStructure(ptr, typeof(T));
+            Marshal.FreeHGlobal(ptr);
+            return structureObj;
         }
     }
 }
